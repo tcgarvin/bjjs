61c77dd057e0152763744f270004b7d4f036b348
Tim Garvin <tcgarvin@gmail.com>
2015-06-21 21:59:44 -0400

    Trying out an index file for use as an entry point.

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  player: require('src/player.js'),
+  blackjack: require('src/blackjack.js'),
+  strategy: {
+    basic: require('src/strategy/BasicStrategy.js'),
+    dealer: require('src/strategy/BasicStrategy.js')
+  }
+};
